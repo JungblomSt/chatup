62bd26fa85c313666f047bb15f04cc5acfd64e83
PatNoO <Noordh94@hotmail.se>
2025-12-23 16:16:06 +0100

    Added minor changes

--- app/src/main/java/com/example/chatup/LoginActivity.kt
+++ app/src/main/java/com/example/chatup/LoginActivity.kt
@@ -16,12 +16,6 @@ class LoginActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
 
-
-
-            //RegisterActivity
-            //FriendListActivity
-
-
         }
     }
 }
