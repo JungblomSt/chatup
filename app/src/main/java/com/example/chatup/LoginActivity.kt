7e3f9089334756ac6cb5e1a59641944f9d88390f
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2025-12-26 19:47:28 +0100

    signin() and signUp()

--- app/src/main/java/com/example/chatup/LoginActivity.kt
+++ app/src/main/java/com/example/chatup/LoginActivity.kt
@@ -1,21 +1,69 @@
 package com.example.chatup
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.chatup.databinding.ActivityLoginBinding
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class LoginActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityLoginBinding
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+
+        auth = Firebase.auth
+
+        binding.btnRegisterAl.setOnClickListener {
+            signUp()
 
         }
+        binding.btnLoginAl.setOnClickListener {
+            signIn()
+        }
+    }
+
+    fun signIn() {
+        val email = binding.etEmailAl.text.toString()
+        val password = binding.etPasswordAl.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            return
+        }
+
+        auth.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener {task ->
+                if (task.isSuccessful){
+                    Log.d("!!!", "login success")
+                } else{
+                    Log.d("!!!", "user not logged in ${task.exception}")
+                }
+            }
+
+    }
+
+    fun signUp() {
+        val email = binding.etEmailAl.text.toString()
+        val password = binding.etPasswordAl.text.toString()
+
+        if (email.isEmpty() || password.isEmpty()) {
+            return
+        }
+
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener {task ->
+                if (task.isSuccessful){
+                    Log.d("!!!", "create success")
+                } else{
+                    Log.d("!!!", "user not created ${task.exception}")
+                }
+            }
+
     }
 }
