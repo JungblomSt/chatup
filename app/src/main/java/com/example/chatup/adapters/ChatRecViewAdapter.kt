41ed1da300eb4f4602aa4254fa8c748547708419
PatNoO <Noordh94@hotmail.se>
2025-12-28 11:56:45 +0100

    Added basic logic in ChatRecViewAdapter

--- app/src/main/java/com/example/chatup/adapters/ChatRecViewAdapter.kt
+++ app/src/main/java/com/example/chatup/adapters/ChatRecViewAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.chatup.adapters
 
-class ChatRecViewAdapter {
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatup.databinding.ItemConversationListLayoutBinding
+
+class ChatRecViewAdapter : RecyclerView.Adapter<ChatRecViewAdapter.ChatViewHolder>() {
+
+    // <Strings> läggs sålänge för att slippa varningar,  antar att det får specificera en skapad class sen istället
+    val chatList = emptyList<String>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): ChatViewHolder {
+        val binding = ItemConversationListLayoutBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return ChatViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(
+        holder: ChatViewHolder,
+        position: Int
+    ) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return chatList.size
+    }
+
+    // Todo får ändra vart bindingen hämtas ifrån sen när vi skapat den layout filen
+    //  - "ItemConversationListLayoutBinding" är då bara tillfälligt
+    inner class ChatViewHolder (val binding : ItemConversationListLayoutBinding) : RecyclerView.ViewHolder (binding.root)
 }
