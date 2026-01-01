46a5a2513dff9229d119776d1937c82534b646e1
Amandamandaanda <amandatanaskovic@gmail.com>
2026-01-01 22:45:54 +0100

    Added callback function inside ConversationListAdatper so  when you click on a coversation list item you will be sent to that conversation

--- app/src/main/java/com/example/chatup/fragments/ConversationListFragment.kt
+++ app/src/main/java/com/example/chatup/fragments/ConversationListFragment.kt
@@ -1,12 +1,15 @@
 package com.example.chatup.fragments
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.View
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.chatup.R
+import com.example.chatup.activitys.ChatActivity
 import com.example.chatup.adapters.ConversationListAdapter
 import com.example.chatup.viewmodel.ConversationListViewModel
 
@@ -22,7 +25,15 @@ class ConversationListFragment : Fragment(R.layout.fragment_conversation_list) {
         val recycler = view.findViewById<RecyclerView>(R.id.conversationListRecycler)
         recycler.layoutManager = LinearLayoutManager(requireContext())
 
-        adapter = ConversationListAdapter(emptyList())
+        adapter = ConversationListAdapter(emptyList()) { conversation ->
+            //TODO: chat acitivy needs to get the conversation id so it can fetch the conversation from firebase
+            //conversation.conversationId
+            Log.d("conversation id", "Conversation id is: ${conversation.conversationId}")
+            val intent = Intent(requireContext(), ChatActivity::class.java)
+            //intent.putExtra("conversationId", conversation.conversationId)
+            startActivity(intent)
+        }
+
         recycler.adapter = adapter
 
         conversationListViewModel.conversationList.observe(viewLifecycleOwner) {
