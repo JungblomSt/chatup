454ac9d9bb84e00baf0bcec33c335bd0e586af6d
Stina Thun <3kdapp25.thunst@folkuniversitetet.nu>
2026-01-05 00:43:37 +0100

    register() buggfix, moved to Repository

--- app/src/main/java/com/example/chatup/AuthRepository.kt
+++ app/src/main/java/com/example/chatup/AuthRepository.kt
@@ -1,17 +1,53 @@
 package com.example.chatup
 
+import com.google.android.gms.tasks.Task
+import com.google.firebase.Firebase
+import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.firestore
 
 class AuthRepository {
 
     private val auth: FirebaseAuth = FirebaseAuth.getInstance()
+
+    fun register(
+        email: String,
+        password: String,
+        onResult: (Task<AuthResult>) -> Unit
+    ) {
+        auth.createUserWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+
+                if (!task.isSuccessful) {
+                    onResult(task)
+                    return@addOnCompleteListener
+                }
+
+                val uid = task.result.user?.uid ?:
+                return@addOnCompleteListener
+
+                val user = hashMapOf(
+                    "uid" to uid,
+                    "email" to email,
+                    "username" to email.substringBefore("@")
+                )
+
+                Firebase.firestore
+                    .collection("users")
+                    .document(uid)
+                    .set(user)
+                    .addOnCompleteListener {
+                        onResult(task)
+                    }
+            }
+    }
 
 
     fun sendPasswordReset(
         email: String,
         onResult: (Boolean, String?) -> Unit
     ){
-//        auth.setLanguageCode("sv") TODO: stopped working when I modified the mail on firebase.
+        auth.setLanguageCode("sv")
         auth.sendPasswordResetEmail(email)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
--- app/src/main/java/com/example/chatup/viewmodel/AuthViewModel.kt
+++ app/src/main/java/com/example/chatup/viewmodel/AuthViewModel.kt
@@ -8,7 +8,6 @@ import com.google.android.gms.tasks.Task
 import com.google.firebase.Firebase
 import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.auth
-import com.google.firebase.firestore.firestore
 
 class AuthViewModel : ViewModel() {
     val auth = Firebase.auth
@@ -20,33 +19,9 @@ class AuthViewModel : ViewModel() {
     fun register(
         email: String,
         password: String,
-        callback: (Task<AuthResult>)-> Unit
-    ){
-        auth.createUserWithEmailAndPassword(email, password)
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    val uid = task.result.user?.uid
-                    if (uid == null) {
-                        callback(task)
-                        return@addOnCompleteListener
-                    }
-
-                    val user = mapOf(
-                        "email" to email,
-                        "username" to email.substringBefore("@")
-                    )
-
-                    Firebase.firestore
-                        .collection("users")
-                        .document(uid)
-                        .set(user)
-                        .addOnCompleteListener {
-                            callback(task)
-                        }
-                } else {
-                    callback(task)
-                }
-            }
+        callback: (Task<AuthResult>) -> Unit
+    ) {
+        repository.register(email, password, callback)
     }
 
     fun login(
